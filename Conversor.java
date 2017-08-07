5821935f04460a756b6fd33f8166510ec3f579c7
Ricardo Amorim <ricardoamorim00@gmail.com>
2017-08-07 11:30:11 -0300

    Implementando metodo KM_Milha

--- Conversor.java
+++ Conversor.java
@@ -5,6 +5,11 @@ public class Conversor{
 
 	}	
 
+	public static void Km_Milha(float km){
+		float milha=km*0.62137;
+		System.out.println(km + "km em milhas: " + milha);
+
+	}
 
 	public static void main(String[] args){
 		System.out.println("Ola mundo!!");
