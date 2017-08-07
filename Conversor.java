2f922c32019defef94a384842ad4f4a11c60f254
Ricardo Amorim <ricardoamorim00@gmail.com>
2017-08-07 11:02:37 -0300

    Iniciando Projeto

---
+++ Conversor.java
@@ -0,0 +1,15 @@
+public class Conversor{
+
+	public static void ola(String nome){
+		System.out.println("Bom dia " + nome);
+
+	}	
+
+
+	public static void main(String[] args){
+		System.out.println("Ola mundo!!");
+		ola("Ricardo");
+
+	}
+
+}
